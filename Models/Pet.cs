472e02714818bfa8bdbedac971845f1369441e71
Shobha Chejara <shobha.chejara@gmail.com>
2024-11-14 09:54:16 -0600

    Pet Models has created

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -2,10 +2,32 @@ using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System;
+using System.Collections.Generic;
 
-namespace pet_hotel
+namespace pet_hotel.Models
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType : byte
+    {
+        Bulldog
+    }
+    public enum PetColorType : byte
+    {
+
+         black 
+    }
+    public class Pet {
+
+        public int id {get; set;}
+         public string name {get; set;}
+         [ForeignKey("petOwnerid")]
+          public int petOwnerid  {get; set;}
+          [JsonConverter(typeof(JsonStringEnumConverter))] 
+         public int checkedInAt {get; set;}
+
+         public string PetBreedType {get; set;}
+
+         public string PetColorType {get; set;}
+
+
+    }
 }
